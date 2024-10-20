ff130dac7573e9fce69c922339ab706671339b1a
Astriania <singularity.syndicate.eve@gmail.com>
2024-10-20 21:00:46 +0100

    Masks - Invite alliance to mask (2)

--- app/js-test/maskRendering.test.js
+++ app/js-test/maskRendering.test.js
@@ -13,7 +13,7 @@ describe('Mask rendering', () => {
 		});
 		it('should render default personal mask', () => {
 			const result = maskRendering.renderMask(sampleData[1]);
-			assert.equal(result, '<span class="mask" data-mask="96191857.1"><i data-icon="user" class="personal"></i>Private</span>');
+			assert.equal(result, '<span class="mask" data-mask="96191857.1"><i data-icon="user" class="character"></i>Private</span>');
 		});
 		it('should render default corp mask', () => {
 			const result = maskRendering.renderMask(sampleData[2]);
@@ -25,7 +25,7 @@ describe('Mask rendering', () => {
 		});
 		it('should render owned personal mask', () => {
 			const result = maskRendering.renderMask(sampleData[4]);
-			assert.equal(result, '<span class="mask" data-mask="1.0"><i data-icon="user" class="personal"></i>Owned</span>');
+			assert.equal(result, '<span class="mask" data-mask="1.0"><i data-icon="user" class="character"></i>Owned</span>');
 		});
 	});
 	
@@ -41,20 +41,21 @@ describe('Mask rendering', () => {
 			assert.equal(document.getElementById('mask').innerHTML, '<span class="mask" data-mask="98363074.2"><i data-icon="star" class="corporate"></i>Corp</span>');
 		});
 		
-		it('should update mask list, showing owned, active and joined', () => {
+		it('should update mask list, showing owned, active and joined/alliance', () => {
 			maskRendering.update( sampleData, "4.0" );
 			// No 3 because it isn't joined. 4 is here because it's active
 			assert.deepEqual(document.getElementById('mask-menu-mask-list').children.map(n => n.className), [
-				undefined, undefined, undefined, undefined, undefined, undefined, 'active'
+				undefined, undefined, undefined, undefined, undefined, undefined, 'active', undefined
 			]);
 			assert.deepEqual(document.getElementById('mask-menu-mask-list').children.map(n => n.innerHTML), [
 				'<span class="mask" data-mask="0.0"><i data-icon="eye" class="global"></i>Public</span>',
-				'<span class="mask" data-mask="96191857.1"><i data-icon="user" class="personal"></i>Private</span>',
+				'<span class="mask" data-mask="96191857.1"><i data-icon="user" class="character"></i>Private</span>',
 				'<span class="mask" data-mask="98363074.2"><i data-icon="star" class="corporate"></i>Corp</span>',
 				'<span class="mask" data-mask="99005476.3"><i data-icon="star" class="alliance"></i>Alliance</span>',
-				'<span class="mask" data-mask="1.0"><i data-icon="user" class="personal"></i>Owned</span>',			
-				'<span class="mask" data-mask="2.0"><i data-icon="user" class="personal"></i>Personal</span>',			
+				'<span class="mask" data-mask="1.0"><i data-icon="user" class="character"></i>Owned</span>',			
+				'<span class="mask" data-mask="2.0"><i data-icon="user" class="character"></i>Personal</span>',			
 				'<span class="mask" data-mask="4.0"><i data-icon="star" class="corporate"></i>Joined through corp</span>',
+				'<span class="mask" data-mask="5.0"><i data-icon="star" class="corporate"></i>Invited through alliance</span>',
 			]);			
 		});
 	});
